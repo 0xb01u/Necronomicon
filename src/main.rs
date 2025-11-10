1d4070987f6e23f1378e3411eb0d39014421c60a
Bolu <bolu@tuta.io>
2025-11-10 16:44:44 +0100

    Run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
-use actix_web::{delete, get, post, web, App, HttpResponse, HttpServer};
+use actix_web::{App, HttpResponse, HttpServer, delete, get, post, web};
 use getset::{Getters, Setters};
 use indexmap::IndexMap;
 use serde::{Deserialize, Serialize};
